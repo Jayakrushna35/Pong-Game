09d776c99bf0bb37428b621370eae5ba82b32eee
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2024-06-23 15:00:18 +0530

    final code

--- gamepaddel.py
+++ gamepaddel.py
@@ -1,16 +1,13 @@
 from turtle import Turtle
 
-
 class Paddle(Turtle):
-
-    def __init__(self,position):
-        super().__init__
+    def __init__(self, position):
+        super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid= 5,stretch_len= 1)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
-
 
     def go_up(self):
         new_y = self.ycor() + 20
@@ -19,5 +16,3 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
-
-        
